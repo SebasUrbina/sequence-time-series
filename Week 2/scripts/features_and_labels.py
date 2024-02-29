a9cf2fba182027bfee562a84100fb8838fc72ee1
SebasUrbina <sebastian.urbina@live.cl>
2024-02-29 20:47:41 -0300

    add script: generate features and labels using tensorflow

---
+++ Week 2/scripts/features_and_labels.py
@@ -0,0 +1,20 @@
+# %%
+import tensorflow as tf
+
+dataset = tf.data.Dataset.range(10)
+dataset = dataset.window(5, shift=1, drop_remainder=True)
+dataset = dataset.flat_map(lambda window: window.batch(5))
+dataset = dataset.map(lambda window: (window[:-1], window[-1]))
+dataset = dataset.shuffle(buffer_size=10)
+
+for x,y in dataset:
+    print(x.numpy(), y.numpy())
+
+## Output
+# [1 2 3 4] 5
+# [3 4 5 6] 7
+# [5 6 7 8] 9
+# [4 5 6 7] 8
+# [0 1 2 3] 4
+# [2 3 4 5] 6
+# %%
