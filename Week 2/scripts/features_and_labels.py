ccdda36197b061661206e4cc185c1fc5e00387cd
SebasUrbina <sebastian.urbina@live.cl>
2024-02-29 20:49:39 -0300

    add commented line to create batch

--- Week 2/scripts/features_and_labels.py
+++ Week 2/scripts/features_and_labels.py
@@ -6,6 +6,7 @@ dataset = dataset.window(5, shift=1, drop_remainder=True)
 dataset = dataset.flat_map(lambda window: window.batch(5))
 dataset = dataset.map(lambda window: (window[:-1], window[-1]))
 dataset = dataset.shuffle(buffer_size=10)
+# dataset = dataset.batch(2).prefetch(1) # Create 3 batchs of size 2 
 
 for x,y in dataset:
     print(x.numpy(), y.numpy())
